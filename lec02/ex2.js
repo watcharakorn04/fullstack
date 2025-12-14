972771446fad63cda5a885d559b77ae5d18f73d4
watcharakorn04 <watcharakornc19@gmail.com>
2025-12-15 00:24:28 +0700

    lec02

---
+++ lec02/ex2.js
@@ -0,0 +1,11 @@
+function assignGrade(score) {
+    if (score >= 90) {return "A";
+    } else if (score >= 80) {return "B";
+    } else if (score >= 70) {return "C";
+    } else if (score >= 60) {return "D";
+    } else {return "F";
+    }
+}
+
+console.log(assignGrade(95));
+console.log(assignGrade(55));
